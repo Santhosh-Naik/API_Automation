871147e8bcc9ec4f493fb3e3b2647d8a77359df8
Santhosh Naik <32381825+Santhosh-Naik@users.noreply.github.com>
2022-09-05 13:26:03 +0530

    Add files via upload
    project created for api

---
+++ API_Automation_Telco/src/test/java/Telco_Api_SendSMS.java
@@ -0,0 +1,78 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static io.restassured.RestAssured.given;
+
+public class Telco_Api_SendSMS {
+
+
+    @Test
+    public static void sendRequestForSMS() {
+
+
+        WebDriver driver = new SafariDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        driver.quit();
+      //  sendRequestBody("10");
+
+    }
+
+
+    public static void sendRequestBody(String series) {
+
+        for (int i = 0; i <=10; i++) {
+
+            //MTN = 2348060817482; Other = 2349091751329; Airtel = 2347019781418
+            RestAssured.baseURI = "http://45.35.48.186";
+            String request = "{\n" +
+                    "  \"phoneNumber\": \"2348060817482\",\n" +
+                    "  \"message\": \"Test message from santhosh for MTN-- " + series + i + "\",\n" +
+                    "  \"smsReference\": \"TestMTN_" + series + i + "\"\n" +
+                    "}";
+            Response resp = given().
+                    header("Content-Type", "application/json").
+                    body(request).
+                    when().
+                    post("/api/Notification/SendSms").
+                    then().log().all().assertThat().statusCode(200).
+                    extract().response();
+
+        }
+
+    }
+
+    public static String getResponse(String series) {
+
+        String response = "";
+        for (int i = 0; i <= 100; i++) {
+
+            response = "{\n" +
+                    "  \"smsReference\": \"T_" + series + i + "\"\n" +
+                    "}";
+        }
+
+        return response;
+
+    }
+
+}
+
+
+ /*  RestAssured.baseURI = "http://45.35.48.186";
+
+        Response resp = given().
+                header("Content-Type", "application/json").
+                body(sendRequestBody("5")).
+                when().
+                post("/api/Notification/SendSms").
+                then().assertThat().statusCode(200).
+                extract().response();
+
+    */
---
+++ API_Automation_Telco/src/test/java/Telco_Api_VerifySent_SMS.java
@@ -0,0 +1,49 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Telco_Api_VerifySent_SMS {
+
+
+    @Test
+    public static void verifyResponseOfSentSMS() {
+
+        getResponse("100");
+
+    }
+
+
+    public static void getResponse(String series) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            RestAssured.baseURI = "http://45.35.48.186";
+            String response = "{\n" +
+                    "  \"smsReference\": \"TestAirtel_" + series + i + "\"\n" +
+                    "}";
+            String resp = given().
+                    header("Content-Type", "application/json").
+                    body(response).
+                    when().
+                    post("/api/Notification/SmsQuery").
+                    then().assertThat().statusCode(200).
+                    extract().response().asString();
+            System.out.println(resp);
+
+
+          /*  JsonPath js = new JsonPath(resp);
+            String ref_id = js.get("smsReference");
+            System.out.println("ref_id: "+ref_id);
+            String sent_SMS = js.get("message");
+            System.out.println("sent_SMS: "+sent_SMS);
+            String sent_SMS_Status = js.get("sent");
+            System.out.println("sent_SMS_Status: "+sent_SMS_Status);
+*/
+        }
+
+    }
+
+}
